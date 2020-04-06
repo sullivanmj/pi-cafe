e17272176522336f967b0028865735df87076cae
Matt Sullivan <50746840+sullivanmj@users.noreply.github.com>
2020-04-05 22:55:55 -0500

    add picafe initial implementation

---
+++ picafe/picafe.py
@@ -0,0 +1,20 @@
+from sys import exit
+
+
+def main():
+    """
+
+    """
+    print('Welcome to Pi Cafe.')
+    print('Starting brew cycle.')
+
+
+def init():
+    """
+
+    """
+    if __name__ == '__main__':
+        exit(main())
+
+
+init()
