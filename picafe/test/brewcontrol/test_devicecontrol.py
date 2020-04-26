8815ff88aa1f7ef55e7c51080de70c8e22b6359f
Matt Sullivan <50746840+sullivanmj@users.noreply.github.com>
2020-04-25 20:12:28 -0500

    add comments to tests

--- picafe/test/brewcontrol/test_devicecontrol.py
+++ picafe/test/brewcontrol/test_devicecontrol.py
@@ -18,6 +18,7 @@ def test_wemodevicewrapper_gets_correct_name():
 
 
 def test_wemodevicewrapper_turn_on_turns_on_device_when_off():
+    # arrange
     mock_wemo_device = mock.Mock(pywemo.WeMoDevice)
     on_func = mock.Mock()
     mock_wemo_device.on = on_func
@@ -25,36 +26,45 @@ def test_wemodevicewrapper_turn_on_turns_on_device_when_off():
     mock_wemo_device.get_state = get_state_func
     wemo_wrapper = devicecontrol.WemoDeviceWrapper(mock_wemo_device)
 
+    # act
     wemo_wrapper.power_on()
 
+    # assert
     on_func.assert_called_once()
 
 
 def test_wemodevicewrapper_turn_on_does_nothing_when_on():
+    # arrange
     mock_wemo_device = mock.Mock(pywemo.WeMoDevice)
     on_func = mock.Mock()
     mock_wemo_device.on = on_func
     mock_wemo_device.get_state = mock.Mock(return_value=devicecontrol.STATE_ON)
     wemo_wrapper = devicecontrol.WemoDeviceWrapper(mock_wemo_device)
 
+    # act
     wemo_wrapper.power_on()
 
+    # assert
     on_func.assert_not_called()
 
 
 def test_wemodevicewrapper_turn_off_turns_off_device_when_on():
+    # arrange
     mock_wemo_device = mock.Mock(pywemo.WeMoDevice)
     off_func = mock.Mock()
     mock_wemo_device.off = off_func
     mock_wemo_device.get_state = mock.Mock(return_value=devicecontrol.STATE_ON)
     wemo_wrapper = devicecontrol.WemoDeviceWrapper(mock_wemo_device)
 
+    # act
     wemo_wrapper.power_off()
 
+    # assert
     off_func.assert_called_once()
 
 
 def test_wemodevicewrapper_turn_off_does_nothing_when_off():
+    # arrange
     mock_wemo_device = mock.Mock(pywemo.WeMoDevice)
     off_func = mock.Mock()
     mock_wemo_device.off = off_func
@@ -62,6 +72,8 @@ def test_wemodevicewrapper_turn_off_does_nothing_when_off():
     mock_wemo_device.get_state = get_state_func
     wemo_wrapper = devicecontrol.WemoDeviceWrapper(mock_wemo_device)
 
+    # act
     wemo_wrapper.power_off()
 
+    # assert
     off_func.assert_not_called()
