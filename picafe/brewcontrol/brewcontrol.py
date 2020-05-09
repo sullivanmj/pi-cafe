8cd2cc8c745b28f1848c19d0cf6261b0d609bf6c
Matt Sullivan <50746840+sullivanmj@users.noreply.github.com>
2020-05-08 20:47:10 -0500

    fix issue with timer in TimedBrewController

--- picafe/brewcontrol/brewcontrol.py
+++ picafe/brewcontrol/brewcontrol.py
@@ -7,7 +7,7 @@ STATE_BREWING = 1
 
 class TimedBrewController:
     def __init__(self, duration: datetime.timedelta, device_controller):
-        self.timer = Timer(duration.total_seconds, self.stop_brew)
+        self.timer = Timer(duration.total_seconds(), self.stop_brew)
         self.device_controller = device_controller
 
     def start_brew(self):
