0e4dd521573d0c36893af18f8fc104ecd1b7b9ea
Matt Sullivan <50746840+sullivanmj@users.noreply.github.com>
2020-05-12 22:51:09 -0500

    add functionality to main script

--- picafe/picafe.py
+++ picafe/picafe.py
@@ -1,6 +1,8 @@
 """The main script entry-point for pi-cafe."""
 
 import sys
+import picafe.brewcontrol.heatingdevicecontrol as heatingdevicecontrol
+import picafe.brewcontrol.brewcontrol
 
 
 def main():
@@ -11,7 +13,47 @@ def main():
     one WeMo device is detected.
     """
     print('Welcome to Pi Cafe.')
-    print('Starting brew cycle.')
+
+    selected_device = select_heating_device()
+    brew_controller = picafe.brewcontrol.brewcontrol.ManualBrewController(
+        selected_device)
+
+    brew_controller.start_brew()
+
+    input('Press ENTER to stop brewing.')
+
+    brew_controller.stop_brew()
+
+    print('Brew cycle complete.')
+    print('Enjoy your beverage.')
+
+def select_heating_device() -> heatingdevicecontrol.HeatingDevice:
+    """Select a heating device.
+
+    If there is only one heating device detected, then that device will
+    be selected. If there is more than one device, then the user will
+    be prompted to select the desired device.
+    """
+    devices = heatingdevicecontrol.get_heating_devices()
+
+    if devices.count() == 0:
+        return None
+    elif devices.count() == 1:
+        return devices[0]
+    else:
+        return prompt_user_to_select_device(devices)
+
+
+def prompt_user_to_select_device(
+    devices: List[heatingdevicecontrol.HeatingDevice]) \
+        -> heatingdevicecontrol.HeatingDevice:
+    """Prompt the user to select from a list of devices.
+
+    The user can select a device by entering the index presented next
+    to the device, or they can cancel by electing to not enter an
+    index.
+    """
+    pass
 
 
 def initialize_application():
@@ -26,3 +68,7 @@ def initialize_application():
 
 
 initialize_application()
+
+
+class NoDeviceSelectedError(Exception):
+    pass
