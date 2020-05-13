1fe2d228b6a2b5d62b7852f82d3db59b6bd57da2
Matt Sullivan <50746840+sullivanmj@users.noreply.github.com>
2020-05-12 22:10:09 -0500

    add manual brew control class add base class for brew controllers

--- picafe/brewcontrol/brewcontrol.py
+++ picafe/brewcontrol/brewcontrol.py
@@ -1,3 +1,4 @@
+import abc
 import datetime
 import picafe.brewcontrol
 
@@ -9,26 +10,58 @@ STATE_STOPPED = 0
 STATE_BREWING = 1
 
 
-class TimedBrewController:
+class BrewController(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass) -> bool:
+        return (hasattr(subclass, 'start_brew')
+                and callable(subclass.start_brew)
+                and hasattr(subclass, 'cancel_brew')
+                and callable(subclass.cancel_brew)
+                or NotImplemented)
+
+    @abc.abstractmethod
+    def start_brew(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def cancel_brew(self) -> None:
+        raise NotImplementedError
+
+
+class ManualBrewController(BrewController):
+    def __init__(self, device_controller: HeatingDevice) -> None:
+        self.device_controller = device_controller
+
+    def start_brew(self) -> None:
+        self.device_controller.power_on()
+
+    def stop_brew(self) -> None:
+        self.device_controller.power_off()
+
+    def cancel_brew(self) -> None:
+        self.stop_brew()
+
+
+class TimedBrewController(BrewController):
     def __init__(self,
                  duration: datetime.timedelta,
                  device_controller: HeatingDevice) -> None:
-        self.timer = Timer(duration.total_seconds(), self.__stop_brew)
+        self.timer = Timer(duration.total_seconds(), self.__end_brew)
         self.device_controller = device_controller
 
     def start_brew(self) -> None:
         self.device_controller.power_on()
         self.timer.start()
 
-    def __stop_brew(self) -> None:
+    def __end_brew(self) -> None:
         self.device_controller.power_off()
 
     def cancel_brew(self) -> None:
-        self.__stop_brew()
+        self.__end_brew()
         self.timer.cancel()
 
 
-class TemperatureBrewController:
+class TemperatureBrewController(BrewController):
     def __init__(self,
                  fahrenheitTarget: float,
                  device_controller: HeatingDevice) -> None:
@@ -38,8 +71,8 @@ class TemperatureBrewController:
     def start_brew(self) -> None:
         self.device_controller.power_on()
 
-    def __stop_brew(self) -> None:
+    def __end_brew(self) -> None:
         self.device_controller.power_off()
 
     def cancel_brew(self) -> None:
-        self.stop_brew()
+        self.__end_brew()
