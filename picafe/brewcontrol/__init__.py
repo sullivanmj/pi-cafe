446de2c428107a36e1e8861cb38c7f470972ae8e
Matt Sullivan <50746840+sullivanmj@users.noreply.github.com>
2020-05-12 22:19:52 -0500

    add ManualBrewController to exports

--- picafe/brewcontrol/__init__.py
+++ picafe/brewcontrol/__init__.py
@@ -1,4 +1,5 @@
 from .heatingdevicecontrol import get_heating_devices
 from .heatingdevicecontrol import HeatingDevice
 from .heatingdevicecontrol import WemoDeviceWrapper
+from .brewcontrol import ManualBrewController
 from .brewcontrol import TimedBrewController
