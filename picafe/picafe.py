8acb812380b2adf49b4e04f59a03516466280600
Matt Sullivan <50746840+sullivanmj@users.noreply.github.com>
2020-05-12 21:57:56 -0500

    improve documentation and method name

--- picafe/picafe.py
+++ picafe/picafe.py
@@ -1,20 +1,28 @@
+"""The main script entry-point for pi-cafe."""
+
 import sys
 
 
 def main():
-    """
+    """Run a pi-cafe brew cycle.
 
+    The brew cycle will either start the single WeMo device that it
+    detects, or prompt the user to select a WeMo device if more than
+    one WeMo device is detected.
     """
     print('Welcome to Pi Cafe.')
     print('Starting brew cycle.')
 
 
-def init():
-    """
+def initialize_application():
+    """Run the application when called as a script.
 
+    When the module is run as a script by the python interpreter, this
+    method will invoke the :function:`picafe.picafe.main` function.
+    Otherwise, nothing will happen.
     """
     if __name__ == '__main__':
         sys.exit(main())
 
 
-init()
+initialize_application()
--- picafe/test/test_picafe.py
+++ picafe/test/test_picafe.py
@@ -2,10 +2,10 @@ import mock
 import pytest
 
 
-def test_init():
+def test_initialize_application_calls_main():
     from picafe import picafe
     with mock.patch.object(picafe, "main", return_value=42):
         with mock.patch.object(picafe, "__name__", "__main__"):
             with mock.patch.object(picafe.sys, 'exit') as mock_exit:
-                picafe.init()
+                picafe.initialize_application()
                 assert mock_exit.call_args[0][0] == 42
